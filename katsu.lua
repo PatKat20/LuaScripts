b59835a5aeb24220f65730c97fc77b3ad33b04ee
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2022-02-27 20:22:24 -0300

    Update katsu.lua

--- katsu.lua
+++ katsu.lua
@@ -1,3 +1,5 @@
+-- This script is based on Deidara katsu jutsu which explode all the player clones 
+
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_HITCOLOR, COLOR_TEAL)
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_PHYSICALDAMAGE)
@@ -17,20 +19,19 @@ setCombatArea(combat, area1)
 function onCastSpell(cid, var)
 local summons = getCreatureSummons(cid)
 addEvent(function()
-               if isCreature(cid) then
-local position1 = {x=getCreaturePosition(var.number).x+1, y=getCreaturePosition(var.number).y, z=getCreaturePosition(var.number).z}
-doSendMagicEffect(position1, 161) 
-doSendMagicEffect(getCreaturePosition(summon), 16)
-doCombat(cid, combat, var)
-for k=1, #summons do
-  local summonPos = getCreaturePosition(summons[k])
-  
-  doSendMagicEffect({x=summonPos.x+1, y=summonPos.y, z=summonPos.z}, 161)
-  doAreaCombatHealth(summons[k], summonPos, arr1, -35000, -50000, 134)
-  doRemoveCreature(summons[k])
-
-end
-               end
-          end, 100)
+    if isCreature(cid) then
+		local position1 = {x=getCreaturePosition(var.number).x+1, y=getCreaturePosition(var.number).y, z=getCreaturePosition(var.number).z}
+		doSendMagicEffect(position1, 161) 
+		doSendMagicEffect(getCreaturePosition(summon), 16)
+		doCombat(cid, combat, var)
+		--Loop for catch all the summons from player.
+		for k=1, #summons do
+  			local summonPos = getCreaturePosition(summons[k])
+			doSendMagicEffect({x=summonPos.x+1, y=summonPos.y, z=summonPos.z}, 161)
+  			doAreaCombatHealth(summons[k], summonPos, arr1, -35000, -50000, 134)
+ 			doRemoveCreature(summons[k])
+		end
+    end
+end, 100)
 return true
 end
